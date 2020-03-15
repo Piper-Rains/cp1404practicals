8054b4b3c8bd50f9eed873a9e132e3a395360585
Piper Rains <piper.rains@my.jcu.edu,au>
2020-03-15 21:33:26 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,15 +1,29 @@
-
 MIN_LENGTH = 4
 
-print("Please enter a valid password, with a length no less than {}".format(MIN_LENGTH))
-password = input("> ")
 
-while len(password) < MIN_LENGTH:
-    print("Invalid password length")
+def main():
+
+    password = get_password()
+
+    asterisks_password(password)
+
+
+def asterisks_password(password):
+    print("Your password has been approved:")
+    for i in range(len(password)):
+        print("*", end=' ')
+    print()
+
+
+def get_password():
     print("Please enter a valid password, with a length no less than {}".format(MIN_LENGTH))
     password = input("> ")
+    while len(password) < MIN_LENGTH:
+        print("Invalid password length")
+        print("Please enter a valid password, with a length no less than {}".format(MIN_LENGTH))
+        password = input("> ")
+    return password
 
-print("Your password has been approved:")
-for i in range(len(password)):
-    print("*", end=' ')
-print()
+
+main()
+
