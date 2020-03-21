1b857dacfbc376a44d0f61943ba79687df4a3e3a
Piper Rains <piper.rains@my.jcu.edu,au>
2020-03-22 01:24:15 +1000

    Create "Quick Pick" Lottery Ticket Generator

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,29 @@
+import random
+
+NUMBERS_IN_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+
+def main():
+    number_of_quickpicks = int(input("How many quick picks?: "))
+
+    while number_of_quickpicks < 0:
+        print("Your number of quick picks must be greater than zero")
+        number_of_quickpicks = int(input("How many quick picks?: "))
+
+    for i in range(number_of_quickpicks):
+        quickpick = []
+
+        for k in range(NUMBERS_IN_LINE):
+            number = random.randint(MINIMUM, MAXIMUM)
+
+            while number in quickpick:
+                number = random.randint(MINIMUM, MAXIMUM)
+            quickpick.append(number)
+
+        quickpick.sort()
+        print(*quickpick)
+
+
+main()
