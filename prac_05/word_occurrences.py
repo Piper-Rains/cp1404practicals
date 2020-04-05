90f398341a0a4fdbe130c6fed6c36fd65d636050
Piper Rains <piper.rains@my.jcu.edu,au>
2020-04-05 22:48:03 +1000

    Use string formatting to align list of words

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -13,5 +13,6 @@ for word in words:
 words = list(frequency_of_word.keys())
 words.sort()
 
+max_length = max((len(word) for word in words))
 for word in words:
-    print("{} : {}".format(word, frequency_of_word[word]))
+    print("{:{}} : {}".format(word, max_length, frequency_of_word[word]))
