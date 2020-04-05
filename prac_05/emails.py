f5de4f472245a813d63a0f2191d545bb94a281f2
Piper Rains <piper.rains@my.jcu.edu,au>
2020-04-05 23:18:50 +1000

    Using if statement and username function, check with user what name matches their email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,6 +3,12 @@ def main():
     email = input("Email: ")
 
     while email != "":
+        name = get_name_in_email(email)
+        check_name = input("Is your name {}? (Y/N) ".format(name))
+        if check_name.upper() != "Y" and check_name != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
 
 
 def get_name_in_email(email):
