2e60cd2648d28afd6af932ea22e8ce40bd2edc29
Piper Rains <piper.rains@my.jcu.edu,au>
2020-04-05 20:40:47 +1000

    Create hexadecimal colour codes constant dictionary

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,4 @@
+
+COLOUR_CODES = {"RoyalBlue": "#4169e1", "SkyBlue": "#87ceeb", "SlateBlue": "#6a5acd", "SlateGray1": "#c6e2ff",
+                "SpringGreen1": "#00ff7f", "thistle": "#d8bfd8", "tomato1": "#ff6347", "turquoise3": "#00c5cd",
+                "violet": "#ee82ee", "black": "#000000"}
