964a57d198e08c1d38a472283881cab83b655acd
Piper Rains <piper.rains@my.jcu.edu,au>
2020-04-05 22:46:04 +1000

    Change loop to print word counts to a list, so the sort() function can be used to alphabetise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,6 +7,11 @@ for word in words:
     frequency = frequency_of_word.get(word, 0)
     frequency_of_word[word] = frequency + 1
 
-for word, count in frequency_of_word.items():
-    print("{0} : {1}".format(word, count))
+# for word, count in frequency_of_word.items():
+#     print("{0} : {1}".format(word, count))
 
+words = list(frequency_of_word.keys())
+words.sort()
+
+for word in words:
+    print("{} : {}".format(word, frequency_of_word[word]))
