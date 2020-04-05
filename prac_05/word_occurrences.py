5d08cfdc62975bd02a6720bec4ba8790ebc9112f
Piper Rains <piper.rains@my.jcu.edu,au>
2020-04-05 21:58:14 +1000

    Create loop to count the frequency a word appears in user's input text and add to dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,3 +1,9 @@
 
 frequency_of_word = {}
 text = input("Text: ")
+words = text.split()
+
+for word in words:
+    frequency = frequency_of_word.get(word, 0)
+    frequency_of_word[word] = frequency + 1
+
