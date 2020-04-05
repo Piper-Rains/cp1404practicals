9087fa9c6b2deacb0cae93486a48629277fb8da3
Piper Rains <piper.rains@my.jcu.edu,au>
2020-04-05 22:39:30 +1000

    Create loop to print the counts of how many of each word are in the file

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,3 +7,6 @@ for word in words:
     frequency = frequency_of_word.get(word, 0)
     frequency_of_word[word] = frequency + 1
 
+for word, count in frequency_of_word.items():
+    print("{0} : {1}".format(word, count))
+
