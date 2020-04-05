1a874058e6abbc5df717bf5a161d913a7fa0dbd1
Piper Rains <piper.rains@my.jcu.edu,au>
2020-04-05 23:19:56 +1000

    Create loop to print the user's email and name stored in the dictionary

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -10,6 +10,9 @@ def main():
         email_to_name[email] = name
         email = input("Email: ")
 
+    for email, name in email_to_name.items():
+        print("{0} ({1})".format(name, email))
+
 
 def get_name_in_email(email):
     username = email.split('@')[0]
