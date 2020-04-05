221730bbe6d765727cdc6a88cd8bb7717fab0201
Piper Rains <piper.rains@my.jcu.edu,au>
2020-04-05 23:55:04 +1000

    Refactor dictionary name to make more meaningful (as result of code review)

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,18 +1,18 @@
 
-frequency_of_word = {}
+word_to_frequency = {}
 text = input("Text: ")
 words = text.split()
 
 for word in words:
-    frequency = frequency_of_word.get(word, 0)
-    frequency_of_word[word] = frequency + 1
+    frequency = word_to_frequency.get(word, 0)
+    word_to_frequency[word] = frequency + 1
 
 # for word, count in frequency_of_word.items():
 #     print("{0} : {1}".format(word, count))
 
-words = list(frequency_of_word.keys())
+words = list(word_to_frequency.keys())
 words.sort()
 
 max_length = max((len(word) for word in words))
 for word in words:
-    print("{:{}} : {}".format(word, max_length, frequency_of_word[word]))
+    print("{:{}} : {}".format(word, max_length, word_to_frequency[word]))
