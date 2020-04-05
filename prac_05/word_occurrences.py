4d0365805df27a980e84485fc01135b694f85b1d
Piper Rains <piper.rains@my.jcu.edu,au>
2020-04-05 20:43:17 +1000

    Write code to allow user to enter colour name and get its code

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,3 +2,12 @@
 COLOUR_CODES = {"RoyalBlue": "#4169e1", "SkyBlue": "#87ceeb", "SlateBlue": "#6a5acd", "SlateGray1": "#c6e2ff",
                 "SpringGreen1": "#00ff7f", "thistle": "#d8bfd8", "tomato1": "#ff6347", "turquoise3": "#00c5cd",
                 "violet": "#ee82ee", "black": "#000000"}
+print(COLOUR_CODES)
+
+colour_name = input("Enter colour name: ")
+while colour_name != "":
+    if colour_name in COLOUR_CODES:
+        print(colour_name, "has code:", COLOUR_CODES[colour_name])
+    else:
+        print("Invalid colour name entered")
+    colour_name = input("Enter colour name: ")
