6d039a24d2eb392a4f9346add26015b290cf22cd
Piper Rains <piper.rains@my.jcu.edu,au>
2020-05-10 21:40:45 +1000

    Add round() function to round the taxi fare to the nearest 10 cents

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -4,7 +4,7 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 def main():
 
-    taxi = SilverServiceTaxi("Taxi", 100, 2)
+    taxi = SilverServiceTaxi("Taxi", 18, 2)
     taxi.drive(18)
     print(taxi)
     print(taxi.get_fare())
